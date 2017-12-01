2ed6c90b7c572ecaebdba3b105c4d29faa7fe234
Daniel Huang <gn03249822@DanieltekiMacBook-Pro.local>
2017-11-30 23:47:35 -0800

    DONE

--- website_blocker.py
+++ website_blocker.py
@@ -6,7 +6,7 @@ redirect = "127.0.0.1"
 web_sites_list = ["www.facebook.com", "facebook.com"]
 
 while True:
-    if dt(dt.now().year, dt.now().month, dt.now().day, 10) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 21):
+    if dt(dt.now().year, dt.now().month, dt.now().day, 9) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day,22):
         print("Working hours")
         with open(hosts_path, "r+") as file:
             content = file.read()
